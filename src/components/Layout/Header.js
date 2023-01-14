c84be089d51f2b97b61ae0d10a7eb73462ee3084
aniiaham <aniya_axm@icloud.com>
2023-01-13 21:29:27 -0500

    still needs to fix the bug

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -8,7 +8,7 @@ const Header = (props) => {
   return <React.Fragment>
     <header className={classes.header}>
         <h1>Samokat</h1>
-       <HeaderCartButton />
+       <HeaderCartButton onClick={props.onShowCart}/>
     </header>
     <div className={classes['main-image']}>
         <img src={samokatImage} alt="Delicious food"/>
